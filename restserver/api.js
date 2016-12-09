7960f745454fe161d909f62ef4baf9e44ac0b3e3
Jake Bladt <jake@ambulnz.com>
2016-12-09 15:08:07 -0500

    return OK to Amazon health monitor

--- restserver/api.js
+++ restserver/api.js
@@ -1,8 +1,16 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 9001;
 
+router.get('/', function(req, res) {
+   res.sendStatus(200);
+});
+
+app.listen(port);
+console.log('listening on port ' + port);
