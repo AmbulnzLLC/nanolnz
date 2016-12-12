cd439bfbdb06abfd4ffa316c6db606c82c52cbe9
Jake Bladt <jake@ambulnz.com>
2016-12-12 15:45:50 -0500

    fix casing

--- webclient/client.js
+++ webclient/client.js
@@ -1,6 +1,6 @@
 const Express = require('express');
-const App = express();
-app.use(Express.static('public'));
+const App = Express();
+App.use(Express.static('public'));
 
 const PORT_NUMBER = process.env.PORT_NUMBER || 5555;
 App.listen(PORT_NUMBER);
