ed63bd7c8a475b617d033131eec66f5835804d9b
Jake Bladt <jake@ambulnz.com>
2016-12-12 15:41:59 -0500

    +index

--- webclient/client.js
+++ webclient/client.js
@@ -2,5 +2,5 @@ const Express = require('express');
 const App = express();
 app.use(Express.static('public'));
 
-const PORT_NUMBER = process.env.PORT_NUMBER || 8888;
+const PORT_NUMBER = process.env.PORT_NUMBER || 5555;
 App.listen(PORT_NUMBER);
