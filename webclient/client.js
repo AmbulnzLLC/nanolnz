3cd81afd204b1f558965e98a2bb0a3a1119c0780
Jake Bladt <jake@ambulnz.com>
2016-12-12 15:53:54 -0500

    use serve-static

--- webclient/client.js
+++ webclient/client.js
@@ -1,6 +1,14 @@
-const Express = require('express');
-const App = Express();
-App.use(Express.static('client'));
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder
+var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']})
+
+// Create server
+var server = http.createServer(function onRequest (req, res) {
+    serve(req, res, finalhandler(req, res))
+})
 
 const PORT_NUMBER = process.env.PORT_NUMBER || 5555;
 App.listen(PORT_NUMBER);
