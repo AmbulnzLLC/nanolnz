d12293560859d5de7d0525ddc8be07857a7369d2
Jake Bladt <jake@ambulnz.com>
2016-12-13 14:54:18 -0500

    log requests

--- restserver/api.js
+++ restserver/api.js
@@ -13,6 +13,7 @@ app.all('/*', function(req, res, next) {
 const port = process.env.PORT || 9001;
 
 router.use(function(req, res, next) {
+    console.log('request made', req);
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
@@ -25,6 +26,7 @@ router.get('/', function(req, res) {
 app.use('/', router);
 
 router.route('/appinfo').get(function(req, res) {
+    console.log('request made to appinfo', req);
     res.json({
         name: 'nanolnz.restserver',
         version: '1.0.0'
