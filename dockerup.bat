f0484841d34a5a11a48dc45d97bab984498a03bd
Jake Bladt <jake@jakebladt.com>
2016-12-16 13:35:48 -0500

    build + launch

--- dockerup.bat
+++ dockerup.bat
@@ -4,3 +4,5 @@ SET DOCKER_HOST=tcp://192.168.99.100:2376
 SET DOCKER_CERT_PATH=C:\Users\jake\.docker\machine\machines\default
 SET DOCKER_MACHINE_NAME=default
 ECHO ON
+docker-compose build
+docker-compose up 
